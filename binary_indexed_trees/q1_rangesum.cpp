52b3eb70558463962d10aa93832062da7011efef
luv <luvk1412@gmail.com>
2020-08-06 09:16:35 +0530

    added q2

--- binary_indexed_trees/q1_rangesum.cpp
+++ binary_indexed_trees/q1_rangesum.cpp
@@ -1,3 +1,30 @@
+/*
+Question 1 Given array of size N and Q querries
+Solve two type of quereies
+Type1 : i x : replace ith index by value x
+Type 2 : l r : find sum from l to r
+Input :
+N Q
+N elemnts of array
+Type i x / Type l r(Q lines)
+
+
+Example :
+5 4
+3 4 5 6 3
+2 2 4
+1 2 6
+2 2 4
+2 1 5
+Output : 
+15
+17
+23
+
+
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
